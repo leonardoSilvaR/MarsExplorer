ca5ef7f006b53b45c07c3b8e0269a6b1337b6fb2
leonardoSilvaR <leonardo.silva.rodrigues2@gmail.com>
2017-12-06 14:01:18 -0200

    First

---
+++ src/main/java/com/br/inpe/marsexplorer/Main.java
@@ -0,0 +1,17 @@
+package com.br.inpe.marsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Leonardo S. Rodrigues <leonardo.silva.rodrigues2@gmail.com>
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
